de875fc051f586ad4738f666c8febb1556f044f1
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2022-05-26 14:02:32 +0200

    cleanup start from fresh

--- src/main/scala/pods/atomic/ReactiveStreams.scala
+++
@@ -1,7 +0,0 @@
-package pods.atomic
-
-/** Reactive Streams interface */
-trait Processor[T, S] extends java.util.concurrent.Flow.Processor[T, S]
-trait Publisher[T] extends java.util.concurrent.Flow.Publisher[T]
-trait Subscriber[T] extends java.util.concurrent.Flow.Subscriber[T]
-trait Subscription extends java.util.concurrent.Flow.Subscription
--- src/main/scala/pods/workflows/TaskBehavior.scala
+++
@@ -1,9 +0,0 @@
-package pods.workflows
-
-private[pods] trait TaskBehavior[I, O]:
-  def onNext(tctx: TaskContext[I, O])(t: I): TaskBehavior[I, O] = ???
-  def onError(tctx: TaskContext[I, O])(t: Throwable): TaskBehavior[I, O] = ???
-  def onComplete(tctx: TaskContext[I, O]): TaskBehavior[I, O] = ???
-  // when alignment has been reached
-  def onAtomComplete(tctx: TaskContext[I, O]): TaskBehavior[I, O] = ???  // onTick
-
