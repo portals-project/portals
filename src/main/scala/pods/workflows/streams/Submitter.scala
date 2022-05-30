047a8b49302d8806a4aa02e185c527bbdabd2e86
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2022-05-30 15:13:06 +0200

    Added FT methods for Submitter

--- src/main/scala/pods/workflows/streams/Submitter.scala
+++ src/main/scala/pods/workflows/streams/Submitter.scala
@@ -7,3 +7,15 @@ trait Submitter[T]:
   private[pods] def seal(): Unit
   
   private[pods] def fuse(): Unit
+
+  // // TODO: implement for fault-tolerance
+
+  // private[pods] def precommit(): Long // returns commit id
+
+  // private[pods] def isCommitted(commit_id: Long): Boolean // check if commit was executed
+  
+  // private[pods] def commit(): Unit // block until commit is completed
+  
+  // private[pods] def commitAsync(): Unit // commit async
+  
+  // private[pods] def rollback(): Unit // trigger rollback
