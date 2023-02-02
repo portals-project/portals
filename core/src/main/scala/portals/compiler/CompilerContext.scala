15f73c930f358b733928afdac4348cad6eec39d5
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-02-02 16:07:19 +0100

    compiler init

---
+++ core/src/main/scala/portals/compiler/CompilerContext.scala
@@ -0,0 +1,4 @@
+package portals
+
+class CompilerContext:
+  def log(msg: String): Unit = println(msg)
---
+++ core/src/main/scala/portals/compiler/CompilerImpl.scala
@@ -0,0 +1,8 @@
+package portals
+
+private[portals] class CompilerImpl extends Compiler[Application, Application]:
+  def compile(t: Application): Application =
+    // semantic analysis
+    ???
+
+end CompilerImpl // class
