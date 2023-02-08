451af330de540bf79d405e8f545c804bfa1b0ca1
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-02-08 18:36:48 +0100

    Update Shopping Cart App Code

--- distributed-examples/src/main/scala/portals/examples/distributed/shoppingcart/ShoppingCart.scala
+++ distributed-examples/src/main/scala/portals/examples/distributed/shoppingcart/ShoppingCart.scala
@@ -4,6 +4,93 @@ import scala.annotation.experimental
 
 import portals.*
 
+@experimental
+object ShoppingCartTasks:
+  import portals.DSL.*
+  import portals.DSL.BuilderDSL.*
+  import portals.DSL.ExperimentalDSL.*
+
+  import ShoppingCart.Events.*
+
+  def cart(
+      portal: AtomicPortalRef[InventoryReqs, InventoryReps]
+  ): GenericTask[CartOps, OrderOps, InventoryReqs, InventoryReps] =
+    TaskBuilder
+      .init[CartOps, OrderOps] {
+        val state = PerKeyState[CartState]("state", CartState.zero)
+        TaskBuilder
+          .portal(portal)
+          .asker[CartOps, OrderOps] { event =>
+            event match
+              case AddToCart(user, item) =>
+                val resp = portal.ask(Get(item))
+                resp.await {
+                  resp.value.get match
+                    case GetReply(item, success) =>
+                      if success then
+                        state.set(state.get().add(item))
+                        ctx.log.info(s"User $user added $item to cart")
+                      else ctx.log.info(s"User $user tried to add $item to cart, but it was not in inventory")
+                    case _ => ctx.log.info("Unexpected response"); ???
+                }
+              case RemoveFromCart(user, item) =>
+                portal.ask(Put(item))
+                ctx.log.info(s"User $user removed $item from cart")
+              case Checkout(user) =>
+                val cart = state.get()
+                ctx.emit(Order(user, cart))
+                state.del()
+                ctx.log.info(s"Checking out $user with cart $cart")
+          }
+          .asInstanceOf[GenericTask[
+            CartOps,
+            OrderOps,
+            Nothing,
+            Nothing
+          ]] // TODO: make it so that we don't have to do this :((
+      }
+      .asInstanceOf[GenericTask[
+        CartOps,
+        OrderOps,
+        InventoryReqs,
+        InventoryReps
+      ]] // TODO: make it so that we don't have to do this :((
+
+  def inventory(
+      portal: AtomicPortalRef[InventoryReqs, InventoryReps]
+  ): GenericTask[InventoryReqs, Nothing, InventoryReqs, InventoryReps] =
+    TaskBuilder
+      .portal(portal)
+      .replier[InventoryReqs, Nothing] {
+        case Get(item) =>
+          val state = PerKeyState[Int]("state", 0)
+          ctx.log.info(s"Taking $item from inventory")
+          if state.get() > 0 then state.set(state.get() - 1) else ???
+        case Put(item) =>
+          ctx.log.info(s"Putting $item in inventory")
+          val state = PerKeyState[Int]("state", 0)
+          state.set(state.get() + 1)
+      } {
+        case Get(item) =>
+          ctx.log.info(s"Checking if $item is in inventory")
+          val state = PerKeyState[Int]("state", 0)
+          if state.get() > 0 then
+            reply(GetReply(item, true))
+            state.set(state.get() - 1)
+          else reply(GetReply(item, false))
+        case Put(item) =>
+          ctx.log.info(s"Putting $item in inventory")
+          val state = PerKeyState[Int]("state", 0)
+          state.set(state.get() + 1)
+          reply(PutReply(item, true))
+      }
+
+  def orders(): GenericTask[OrderOps, Nothing, Nothing, Nothing] =
+    TaskBuilder.flatMap[OrderOps, Nothing] { event =>
+      ctx.log.info(s"Ordering $event")
+      List.empty
+    }
+
 ////////////////////////////////////////////////////////////////////////////////
 // shopping cart app
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,37 +100,52 @@ object ShoppingCart:
   import portals.DSL.BuilderDSL.*
   import portals.DSL.ExperimentalDSL.*
 
-  // operations
-  // cart
-  sealed trait CartOps:
-    def key: Long
-  @experimental case class AddToCart(user: Int, item: Int) extends CartOps:
-    override def key: Long = user
-  @experimental case class RemoveFromCart(user: Int, item: Int) extends CartOps:
-    override def key: Long = user
-  @experimental case class Checkout(user: Int) extends CartOps:
-    override def key: Long = user
+  //////////////////////////////////////////////////////////////////////////////
+  // Events
+  //////////////////////////////////////////////////////////////////////////////
+  object Events:
+    ////////////////////////////////////////////////////////////////////////////
+    // Cart
+    ////////////////////////////////////////////////////////////////////////////
+    sealed trait CartOps
+    @experimental case class AddToCart(user: Int, item: Int) extends CartOps
+    @experimental case class RemoveFromCart(user: Int, item: Int) extends CartOps
+    @experimental case class Checkout(user: Int) extends CartOps
 
-  // cart state
-  @experimental case class CartState(items: List[Int]):
-    def add(item: Int): CartState = CartState(item :: items)
-    def remove(item: Int): CartState = CartState(items.diff(List(item)))
-  object CartState { def zero: CartState = CartState(List.empty) }
+    @experimental case class CartState(items: List[Int]):
+      def add(item: Int): CartState = CartState(item :: items)
+      def remove(item: Int): CartState = CartState(items.diff(List(item)))
+    object CartState { def zero: CartState = CartState(List.empty) }
 
-  // inventory
-  sealed trait InventoryReqs:
-    def key: Long
-  @experimental case class Get(item: Int) extends InventoryReqs:
-    def key: Long = item
-  @experimental case class Put(item: Int) extends InventoryReqs:
-    def key: Long = item
-  sealed trait InventoryReps
-  @experimental case class GetReply(item: Int, success: Boolean) extends InventoryReps
-  @experimental case class PutReply(item: Int, success: Boolean) extends InventoryReps
+    ////////////////////////////////////////////////////////////////////////////
+    // Inventory
+    ////////////////////////////////////////////////////////////////////////////
+    sealed trait InventoryReqs
+    @experimental case class Get(item: Int) extends InventoryReqs
+    @experimental case class Put(item: Int) extends InventoryReqs
+    sealed trait InventoryReps
+    @experimental case class GetReply(item: Int, success: Boolean) extends InventoryReps
+    @experimental case class PutReply(item: Int, success: Boolean) extends InventoryReps
 
-  // orders
-  sealed trait OrderOps
-  @experimental case class Order(user: Int, order: CartState) extends OrderOps
+    ////////////////////////////////////////////////////////////////////////////
+    // Orders
+    ////////////////////////////////////////////////////////////////////////////
+    sealed trait OrderOps
+    @experimental case class Order(user: Int, order: CartState) extends OrderOps
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Key
+    ////////////////////////////////////////////////////////////////////////////
+    def keyFrom(cartOp: CartOps): Long = cartOp match
+      case AddToCart(user, _) => user
+      case RemoveFromCart(user, _) => user
+      case Checkout(user) => user
+
+    def keyFrom(inventoryReq: InventoryReqs): Long = inventoryReq match
+      case Get(item) => item
+      case Put(item) => item
+
+  import Events.*
 
   val cartopsList: List[List[CartOps]] =
     List(
@@ -70,88 +172,28 @@ object ShoppingCart:
     val inventoryops = Generators.fromList(inventoryopsList)
     val cartops = Generators.fromListOfLists(cartopsList)
 
-    val portal = Portal[InventoryReqs, InventoryReps]("inventory", { x => x.key })
-    // val portal = Portal[InventoryReqs, InventoryReps]("inventory")
+    val portal = Portal[InventoryReqs, InventoryReps]("inventory", { x => keyFrom(x) })
 
     val cart = Workflows[CartOps, OrderOps]("cart")
       .source(cartops.stream)
-      .key(_.key)
-      .init[OrderOps] {
-        val state = PerKeyState[CartState]("state", CartState.zero)
-        TaskBuilder
-          .portal(portal)
-          .asker[CartOps, OrderOps] { event =>
-            event match
-              case AddToCart(user, item) =>
-                val resp = portal.ask(Get(item))
-                resp.await {
-                  resp.value.get match
-                    case GetReply(item, success) =>
-                      if success then // TODO: state set
-                        state.set(state.get().add(item))
-                        ctx.log.info(s"Item $item added to cart")
-                      else ctx.log.info(s"Could not add $item to cart")
-                    case _ =>
-                      ctx.log.info("Unexpected response")
-                      ???
-                }
-              case RemoveFromCart(user, item) =>
-                portal.ask(Put(item))
-                ctx.log.info(s"Removed $item from cart")
-              case Checkout(user) =>
-                val cart = state.get()
-                ctx.emit(Order(user, cart))
-                state.del()
-                ctx.log.info(s"Checking out $user with cart $cart")
-          }
-          .asInstanceOf[GenericTask[CartOps, OrderOps, Nothing, Nothing]]
-
-        // .asInstanceOf // TODO: make it so that we don't have to do this :((
-      }
+      .key(keyFrom(_))
+      .task(ShoppingCartTasks.cart(portal))
       .withName("cart")
       .sink()
       .freeze()
 
     val inventory = Workflows[InventoryReqs, Nothing]("inventory")
       .source(inventoryops.stream)
-      // .logger()
-      .key(_.key)
-      .portal(portal)
-      .replier[Nothing] {
-        case Get(item) =>
-          ctx.log.info(s"Taking $item from inventory")
-          val state = PerKeyState[Int]("state", 0)
-          if state.get() > 0 then state.set(state.get() - 1) else ???
-        case Put(item) =>
-          ctx.log.info(s"Putting $item in inventory")
-          val state = PerKeyState[Int]("state", 0)
-          state.set(state.get() + 1)
-      } {
-        case Get(item) =>
-          ctx.log.info(s"Checking if $item is in inventory")
-          val state = PerKeyState[Int]("state", 0)
-          if state.get() > 0 then
-            reply(GetReply(item, true))
-            state.set(state.get() - 1)
-          else reply(GetReply(item, false))
-        case Put(item) =>
-          ctx.log.info(s"Putting $item in inventory")
-          val state = PerKeyState[Int]("state", 0)
-          state.set(state.get() + 1)
-          reply(PutReply(item, true))
-      }
+      .key(keyFrom(_))
+      .task(ShoppingCartTasks.inventory(portal))
       .withName("inventory")
       .sink()
       .freeze()
 
     val orders = Workflows[OrderOps, Nothing]("orders")
       .source(cart.stream)
-      // .logger()
-      .map { x =>
-        ctx.log.info(s"Ordering $x")
-      }
+      .task(ShoppingCartTasks.orders())
       .withName("orders")
-      .empty[Nothing]()
       .sink()
       .freeze()
   }
