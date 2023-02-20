af52785cdd105317f69ec3e24b80b8a442187253
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-02-20 18:40:01 +0100

    Added comment on keyrange for Long.MaxValue

--- core/src/main/scala/portals/compiler/physicalplan/PlanConfig.scala
+++ core/src/main/scala/portals/compiler/physicalplan/PlanConfig.scala
@@ -6,7 +6,7 @@ private[portals] sealed trait PlanConfig
 /** Base configuration of a physical plan with a keyrange.
   *
   * @param keyrange
-  *   The keyrange of this plan, right-open, i.e. [start, end), (exclusive end).
+  *   The keyrange of this plan, right-open, i.e. [start, end), (exclusive end) (except for Long.MaxValue).
   */
 private[portals] case class BaseConfig(keyrange: (Long, Long)) extends PlanConfig
 
