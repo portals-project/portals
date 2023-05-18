d09262c928f3e9a56dec83e13b64ae2ab3edc6b6
MKLepium <maximilian.kurzawski@gmail.com>
2023-05-18 11:55:31 +0200

    changed config to allow for ease of testing

--- core/src/main/scala/portals/util/Config.scala
+++ core/src/main/scala/portals/util/Config.scala
@@ -1,15 +1,26 @@
 package portals.util
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
 
 object Config {
   /**
    * Load the application configuration file.
    * Wrapper class in order to have the config loaded only once.
    */
-  private val config: Config = ConfigFactory.load("portals.conf")
+  private var config: Config = ConfigFactory.load("portals.conf")
 
   def getString(key: String): String = {
     config.getString(key)
+  }
+
+  /**
+   * Set a value in the configuration file.
+   * @param key The key to set.
+   * @param value The value to set.
+   * @example setString("state.backend", "MapStateBackendImpl")
+   */
+  def setString(key: String, value: String): Unit = {
+    val configValue = ConfigValueFactory.fromAnyRef(value)
+    config = config.withValue(key, configValue)
   }
 }
