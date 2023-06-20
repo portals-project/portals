fd2967b59dadad7eb41728dc185d1d44c5863de8
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-06-20 17:01:33 +0200

    Testing for PortalsJS (#188)
    * created test driver/runner and test output generator
    * update version on obfuscator
    * delete unnecessary files
    * create corresponding output files

---
+++ portals-js/src/main/resources/test-generate-outputs.js
@@ -0,0 +1,60 @@
+// Run this file to generate the expected outputs of example files if their 
+// outputs do not yet exist.
+
+const fs = require('fs');
+const path = require('path');
+const exampleDirectory = './examples/'; // the directory containing the examples
+const portalsJS = '../../../target/scala-3.3.0-RC6/portals-js.js'; // the PortalsJS library
+const portalsJSCode = fs.readFileSync(portalsJS, 'utf8');
+
+// files for which outputs are not generated
+const ignoredFiles = [
+  'union.js',
+  'splitter.js',
+]
+
+// helper function to capture the output to console.log
+function LogCapture() {
+  var log = "";
+  const consoleLog = console.log;
+  return {
+    startCapturing() { console.log = (msg) => { log += msg + '\n' } },
+    stopCapturing() { console.log = consoleLog },
+    clear() { log = "" },
+    retrieve() { return log },
+  };
+}
+const log = LogCapture();
+
+// read all example files from the example directory, if no output exists, then 
+// create the corresponding output file
+fs.readdirSync(exampleDirectory).forEach((file) => {
+  if (file.endsWith('.js')) {
+    // example
+    const exampleFilePath = path.join(exampleDirectory, file);
+    const example = fs.readFileSync(exampleFilePath, 'utf8');
+    const exampleCode = portalsJSCode + "\n" + example;
+
+    // ignore
+    if (ignoredFiles.includes(file)) {
+      return;
+    }
+
+    const expectedFilePath = path.join(exampleDirectory, `${file}.out`);
+    try {
+      expected = fs.readFileSync(expectedFilePath, 'utf8');
+    } catch (error) {
+      // creating the expected output file if it does not exist
+      console.warn(`Expected output file not found. Creating ${expectedFilePath}`);
+      log.startCapturing();
+      eval(exampleCode);
+      const output = log.retrieve();
+      log.clear();
+      log.stopCapturing();
+      fs.writeFileSync(expectedFilePath, output, 'utf8');
+      return;
+    }
+  }
+});
+
+console.log('All outputs generated!');
---
+++ portals-js/src/main/resources/test-runner.js
@@ -0,0 +1,69 @@
+// The test runner exeutes all the examples in the examples directory and 
+// compares the output to the expected output. If a file is in the 
+// `ignoredOutputs` array, it will be executed, but its output will not be 
+// compared to the expected output. To create the expected outputs, run the file
+// `test-generate-ouputs.js` instead.
+
+const fs = require('fs');
+const path = require('path');
+const exampleDirectory = './examples/'; // the directory containing the examples
+const portalsJS = '../../../target/scala-3.3.0-RC6/portals-js.js'; // the PortalsJS library
+const portalsJSCode = fs.readFileSync(portalsJS, 'utf8');
+
+// files for which the outputs are ignored
+const ignoredOutputs = [
+  'union.js',
+  'splitter.js',
+]
+
+// helper function to capture the output to console.log
+function LogCapture() {
+  var log = "";
+  const consoleLog = console.log;
+  return {
+    startCapturing() { console.log = (msg) => { log += msg + '\n' } },
+    stopCapturing() { console.log = consoleLog },
+    clear() { log = "" },
+    retrieve() { return log },
+  };
+}
+const log = LogCapture();
+
+// read all example files from the example directory, if an output exists, then
+// compare the output to the expected output, otherwise just executed the file
+// and check if it executes without errors
+fs.readdirSync(exampleDirectory).forEach((file) => {
+  if (file.endsWith('.js')) {
+    // example
+    const exampleFilePath = path.join(exampleDirectory, file);
+    const example = fs.readFileSync(exampleFilePath, 'utf8');
+    const exampleCode = portalsJSCode + "\n" + example;
+
+    // execute, verify no errors if no output file exists
+    if (ignoredOutputs.includes(file)) {
+      log.startCapturing();
+      eval(exampleCode);
+      log.clear();
+      log.stopCapturing();
+      return;
+    }
+
+    // expected output
+    const expectedFilePath = path.join(exampleDirectory, `${file}.out`);
+    expected = fs.readFileSync(expectedFilePath, 'utf8');
+
+    // execute
+    log.startCapturing();
+    eval(exampleCode);
+    const actual = log.retrieve();
+    log.clear();
+    log.stopCapturing();
+
+    // verify output
+    if (actual !== expected) {
+      throw new Error(`Expected ${expected}, but got ${actual}`);
+    }
+  }
+});
+
+console.log('All tests passed!');
