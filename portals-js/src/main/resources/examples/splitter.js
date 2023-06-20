c1d0f72509690b229ea13dbf6af89a2b096db334
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-06-20 15:09:04 +0200

    Fix issues with Splitter JS example

--- portals-js/src/main/resources/examples/splitter.js
+++ portals-js/src/main/resources/examples/splitter.js
@@ -1,33 +1,21 @@
 var builder = PortalsJS.ApplicationBuilder("app")
-
 var generator = builder.generators.fromRange(0, 128, 8)
-
 var splitter = builder.splitters.empty(generator.stream)
-
-var split1 = builder.splits.split(splitter, _ % 2 == 0)
-
-var split2 = builder.splits.split(splitter, _ % 2 == 1)
-
-// consumer 1
+var split1 = builder.splits.split(splitter, x => x % 2 == 0)
+var split2 = builder.splits.split(splitter, x => x % 2 == 1)
 var _ = builder
     .workflows
     .source(split1)
-    .map(_=>x)
+    .logger("split 1: ")
     .sink()
     .freeze()
-
-// consumer 2
 var _ = builder
     .workflows
     .source(split2)
-    .map(_=>x)
+    .logger("split 2: ")
     .sink()
     .freeze()
-
 var app = builder.build()
-
 var system = PortalsJS.System()
-
 system.launch(app)
-
 system.stepUntilComplete()
