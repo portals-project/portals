812c9bb07b222f1feb1cb3a2a742d117a0f29c5b
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-08-10 16:26:16 +0200

    Remove old benchmarks for a fresh start (#245)
    * Remove old benchmarks for a fresh start

--- portals-benchmark/src/main/scala/portals/benchmark/systems/SystemsExtension.scala
+++
@@ -1,9 +0,0 @@
-package portals.benchmark.systems
-
-import portals.system.PortalsSystem
-import portals.system.Systems
-import portals.system.TestSystem
-
-extension (systems: Systems) {
-  def default: PortalsSystem = ???
-}
