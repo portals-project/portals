ccb285facb91b0fca28246596c5e3bc112f888b7
Maximilian Georg Kurzawski <52952859+MKLepium@users.noreply.github.com>
2023-08-10 19:54:01 +0200

    Configurable IP and Port for Client and Server (#243)
    * Added option for IP and Port for the Portals Client and Server.
    * Closes #232.
    
    ---------
    
    Co-authored-by: Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>

--- portals-distributed/src/main/scala/portals/distributed/ClientCLI.scala
+++ portals-distributed/src/main/scala/portals/distributed/ClientCLI.scala
@@ -4,10 +4,11 @@ import mainargs.ParserForMethods
 
 /** Command line interface for the client.
   *
-  * Note: to run it from SBT use (don't forget to end with a "):
-  * ```
-  * sbt "distributed/runMain portals.distributed.ClientCLI ..."
-  * ```
+  * @example
+  *   Display the help message to see all available commands:
+  *   {{{
+  * sbt "distributed/runMain portals.distributed.ClientCLI --help"
+  *   }}}
   */
 object ClientCLI:
 
@@ -22,13 +23,34 @@ object ClientCLI:
     *   {{{
     * ClientCLI submit --path portals/distributed/examples/HelloWorld$.class --directory portals-distributed/target/scala-3.3.0/classes
     *   }}}
+    *
+    * @example
+    *   {{{
+    * ClientCLI submit --path portals/distributed/examples/HelloWorld$.class --ip localhost --port 8080
+    *   }}}
+    *
+    * @example
+    *   {{{
+    * ClientCLI submit --path portals/distributed/examples/HelloWorld$.class --directory portals-distributed/target/scala-3.3.0/classes --ip localhost --port 8080
+    *   }}}
+    *
+    * @param path
+    *   the path to the classfile to submit
+    * @param directory
+    *   (optional) the directory containing the classfile to submit
+    * @param ip
+    *   (optional) the IP address of the server
+    * @param port
+    *   (optional) the port of the server
     */
   @mainargs.main
   def submit(
       path: String,
-      directory: String = "."
+      directory: String = ".",
+      ip: String = "localhost",
+      port: Int = 8080
   ): Unit =
-    Client.submitClassFile(path, directory)
+    Client.submitClassFile(path, directory, ip, port)
 
   /** Submit all classfiles within a `directory` to the server.
     *
@@ -36,12 +58,26 @@ object ClientCLI:
     *   {{{
     * ClientCLI submitDir --directory portals-distributed/target/scala-3.3.0/classes
     *   }}}
+    *
+    * @example
+    *   {{{
+    * ClientCLI submitDir --directory portals-distributed/target/scala-3.3.0/classes --ip localhost --port 8080
+    *   }}}
+    *
+    * @param directory
+    *   the directory containing the classfile to submit
+    * @param ip
+    *   (optional) the IP address of the server
+    * @param port
+    *   (optional) the port of the server
     */
   @mainargs.main
   def submitDir(
-      directory: String
+      directory: String,
+      ip: String = "localhost",
+      port: Int = 8080
   ): Unit =
-    Client.submitClassFilesFromDir(directory)
+    Client.submitClassFilesFromDir(directory, ip, port)
 
   /** Launch an application on the server.
     *
@@ -49,12 +85,26 @@ object ClientCLI:
     *   {{{
     * ClientCLI launch --application portals.distributed.examples.HelloWorld$
     *   }}}
+    *
+    * @example
+    *   {{{
+    * ClientCLI launch --application portals.distributed.examples.HelloWorld$ --ip localhost --port 8080
+    *   }}}
+    *
+    * @param application
+    *   the Java path to the application to launch
+    * @param ip
+    *   (optional) the IP address of the server
+    * @param port
+    *   (optional) the port of the server
     */
   @mainargs.main
   def launch(
       application: String,
+      ip: String = "localhost",
+      port: Int = 8080
   ): Unit =
-    Client.launch(application)
+    Client.launch(application, ip, port)
 
   // using the mainargs library
   def main(args: Array[String]): Unit =
